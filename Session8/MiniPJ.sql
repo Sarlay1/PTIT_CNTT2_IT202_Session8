0af72479f4ac504f251d8efa24b97c6efd8c062b
tiendat <dat.70.k3@gmail.com>
2026-01-07 07:46:48 +0700

    Add files via upload

---
+++ Session8/MiniPJ.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE mini_project_ss08;
+USE mini_project_ss08;
+
+-- Xóa bảng nếu đã tồn tại (để chạy lại nhiều lần)
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS guests;
+
+-- Bảng khách hàng
+CREATE TABLE guests (
+    guest_id INT PRIMARY KEY AUTO_INCREMENT,
+    guest_name VARCHAR(100),
+    phone VARCHAR(20)
+);
+
+-- Bảng phòng
+CREATE TABLE rooms (
+    room_id INT PRIMARY KEY AUTO_INCREMENT,
+    room_type VARCHAR(50),
+    price_per_day DECIMAL(10,0)
+);
+
+-- Bảng đặt phòng
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY AUTO_INCREMENT,
+    guest_id INT,
+    room_id INT,
+    check_in DATE,
+    check_out DATE,
+    FOREIGN KEY (guest_id) REFERENCES guests(guest_id),
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
+
+INSERT INTO guests (guest_name, phone) VALUES
+('Nguyễn Văn An', '0901111111'),
+('Trần Thị Bình', '0902222222'),
+('Lê Văn Cường', '0903333333'),
+('Phạm Thị Dung', '0904444444'),
+('Hoàng Văn Em', '0905555555');
+
+INSERT INTO rooms (room_type, price_per_day) VALUES
+('Standard', 500000),
+('Standard', 500000),
+('Deluxe', 800000),
+('Deluxe', 800000),
+('VIP', 1500000),
+('VIP', 2000000);
+
+INSERT INTO bookings (guest_id, room_id, check_in, check_out) VALUES
+(1, 1, '2024-01-10', '2024-01-12'), -- 2 ngày
+(1, 3, '2024-03-05', '2024-03-10'), -- 5 ngày
+(2, 2, '2024-02-01', '2024-02-03'), -- 2 ngày
+(2, 5, '2024-04-15', '2024-04-18'), -- 3 ngày
+(3, 4, '2023-12-20', '2023-12-25'), -- 5 ngày
+(3, 6, '2024-05-01', '2024-05-06'), -- 5 ngày
+(4, 1, '2024-06-10', '2024-06-11'); -- 1 ngày
+
+-- 1 Liệt kê tất cả tên khách và sdt 
+select guest_name, phone from guests;
+
+-- 2 Liệt kê các loại phòng khác nhau trong khách sạn 
+select room_type from rooms;
+
+-- 3 Hiển thị loại phòng và giá thuê sắp theo ngày giá tăng dần 
+select room_type, price_per_day from rooms
+order by price_per_day asc;
+
+-- 4 Hiển thị các phòng có giá thuê > 1.000.000
+select * from rooms where price_per_day > 1000000;
+
+-- 5 iệt kê các lần đặt trong 2024
+select * from bookings where year(check_in) = 2024;
+
+-- 6 Cho biết số lượng phòng từng phòng
+select room_type, count(*) as so_luong_phong 
+from rooms 
+group by room_type;
+
+
+-- PHẦN 2
+-- 7 Danh sách các lần đặt phòng (tên khách - loại phòng - ngày nhận phòng)
+select g.guest_name, r.room_type, b.check_in
+from bookings b
+join guests g on b.guest_id = g.guest_id
+join rooms r on b.room_id = r.room_id;
+
+-- 8 Mỗi khách đã đặt phòng bao nhiêu lần 
+ select g.guest_name, count(b.booking_id) as so_lan_dat
+ from guest g
+ left join bookings b on g.guest_id = b.guest_id
+ group by g.guest_id, g.guest_name;
+ 
+ -- 9 Doanh thu mỗi phòng
+select 
+    b.booking_id,
+    datediff(b.check_out, b.check_in) * r.price_per_day as doanh_thu
+from bookings b
+join rooms r on b.room_id = r.room_id;
+
+-- 10 Tổng doanh thu của từng loại phòng
+-- 11 Những khách đặt 2 phòng trở lên 
+select guest_id, count(*) as so_lan_dat
+from bookings
+group by guest_id
+having count(*) >= 2;
+
+-- 12 Tìm loại phòng có số lượt đặt nhiều nhất 
+select r.room_type, count(*) as so_luot_dat
+from bookings b
+join rooms r on b.room_id = r.room_id
+group by r.room_type
+order by so_luot_dat desc
+limit 1;
+ 
+ 
+ 
+ -- PHẦN 3
+ -- 13 Hiển thị những phòng có giá thuê cao hơn giâ trung bình tất cả các phòng 
+select *
+from rooms
+where price_per_day > (
+    select avg(price_per_day)
+    from rooms
+);
+
+-- 14 
+select *
+from guests
+where guest_id not in (
+    select guest_id
+    from bookings
+);
+
+
+-- 15
+select room_id, count(*) as so_lan_dat
+from bookings
+group by room_id
+order by so_lan_dat desc
+limit 1;
+
+ 
